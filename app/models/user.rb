e32851b9de36924567ce6a0f9d3664d7662c6d0f
Javier Botero <javierbotero10271@gmail.com>
2021-03-03 18:25:11 -0500

    Makes associations for Course and adds User friends method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ApplicationRecord
   has_many :accepted_friendships, -> { where confirmed: true }, class_name: 'Friendship', foreign_key: 'receiver_id', dependent: :destroy
   has_many :pending_requested_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'sender_id', dependent: :destroy
   has_many :pending_accepted_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'receiver_id', dependent: :destroy
-  has_many :requests, through: :pending_accepted_friendships, source: :sender
-  has_many :pendings, through: :pending_requested_friendships, source: :receiver
+  has_many :requests, through: :accepted_friendships, source: :sender
+  has_many :pendings, through: :requested_friendships, source: :receiver
   has_many :favorites
   has_many :subscriptions
   has_many :courses, foreign_key: 'teacher_id'
   has_many :comments
+
+  def friends
+    requests + pendings
+  end
 end
