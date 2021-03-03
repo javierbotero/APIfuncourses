c5c7610a436f686198c27a517b76f7e292919205
Javier Botero <javierbotero10271@gmail.com>
2021-03-03 18:54:52 -0500

    Removing unneccesary associations from Course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :teacher, class_name: 'User', foreign_key: 'teacher_id'
   has_many :favorites, dependent: :destroy
-  has_many :users, through: :favorites
   has_many :pendings, -> { where confirmed: false }, class_name: 'Subscription'
   has_many :subscriptions, -> { where confirmed: true }
-  has_many :users, through: :subscriptions
   has_many :comments
 end
