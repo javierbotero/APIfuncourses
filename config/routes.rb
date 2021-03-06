9da5f62c7a3759f5f0a1528f721c8620457ca762
Javier Botero <javierbotero10271@gmail.com>
2021-03-06 12:24:54 -0500

    Fix routes for courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'courses/index'
-  get 'courses/create'
-  get 'courses/update'
-  get 'courses/delete'
   post '/login', to: 'users#login'
   post '/signup', to: 'users#create'
   resources :users, only: [:update, :destroy]
+  resources :courses, only: [:index, :create, :update, :destroy]
 end
