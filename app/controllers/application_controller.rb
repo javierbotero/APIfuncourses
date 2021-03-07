09e952f63eaee000dbe082d118ddd47ef54995cb
Javier Botero <javierbotero10271@gmail.com>
2021-03-07 12:57:46 -0500

    Fix authentication of user method in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  private
+
+  def authenticate
+    @user = User.find(params[:current_user_id])
+
+    unless @user && @user.authenticate(params[:current_user_password])
+      render json: 'You are not allowed to execute this action', status: 404
+    end
+  end
 end
