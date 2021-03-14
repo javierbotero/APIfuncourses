66cce75c1eba199c71e4bdbd7bcf2077ed1fff95
Javier Botero <javierbotero10271@gmail.com>
2021-03-13 23:58:03 -0500

    Improves associations in user.rb to return friendships data

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :accepted_friendships, -> { where confirmed: true }, class_name: 'Friendship', foreign_key: 'receiver_id', dependent: :destroy
   has_many :pending_requested_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'sender_id', dependent: :destroy
   has_many :pending_accepted_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'receiver_id', dependent: :destroy
+  has_many :friendship_requests, through: :pending_accepted_friendships, source: :sender
   has_many :requests, through: :accepted_friendships, source: :sender
   has_many :pendings, through: :requested_friendships, source: :receiver
   has_many :favorites, dependent: :destroy
@@ -69,6 +70,23 @@ class User < ApplicationRecord
         {
           favorites: {
             only: :course_id
+          }
+        },
+        {
+          pending_accepted_friendships: {
+            except: [
+              :created_at,
+              :updated_at,
+            ]
+          }
+        },
+        {
+          friendship_requests: {
+            only: [
+              :id,
+              :username,
+              :email,
+            ]
           }
         },
         {
