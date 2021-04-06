0dfee44744bd74f9bf8885f8ddbe6f8fd9b8fc70
Javier Botero <javierbotero10271@gmail.com>
2021-04-06 18:18:00 -0500

    Adds test for destroy token

--- spec/requests/tokens_request_spec.rb
+++ spec/requests/tokens_request_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe 'Tokens', type: :request do
     post '/tokens'
     expect(response).to have_http_status(:success)
   end
+
+  it 'Deletes Token' do
+    str = SecureRandom.hex
+    token = Token.create(token: str)
+    delete "/tokens/#{token.id}", params: { token: token, token_id: token.id }
+    expect(JSON.parse(response.body)['response'] == 'Token destroyed, thanks for contributing to clean the Database').to be true
+  end
 end
